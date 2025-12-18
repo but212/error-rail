ea3e970442d193f9415b800984429b3920a2742f
but212 <wjddlf211@naver.com>
2025-12-18 13:50:36 +0900

    bench: add feature gates for serde-dependent imports and update benchmark results

--- benches/benchmark/features.rs
+++ benches/benchmark/features.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "std")]
 use crate::common::UserData;
+#[cfg(feature = "serde")]
 use crate::common::{configure_criterion, DomainError};
+#[cfg(feature = "serde")]
 use criterion::{criterion_group, Criterion};
 #[cfg(feature = "std")]
 use error_rail::ErrorPipeline;
